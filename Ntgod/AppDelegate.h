a73c854397a6fac761c7092754e9e3098894a7a4
hansensen <1181040659@qq.com>
2017-12-10 13:57:26 +0800

    Initial Commit

---
+++ Ntgod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ntgod
+//
+//  Created by 韩森 on 2017/12/10.
+//  Copyright © 2017年 Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ntgod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ntgod
+//
+//  Created by 韩森 on 2017/12/10.
+//  Copyright © 2017年 Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
